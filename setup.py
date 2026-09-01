656e18388dd05ccdb0861dd81bd83337ddf3727c
Duygu Ersoy <duyguersoy606@gmail.com>
2026-09-01 03:12:46 +0300

    Fix MQTT Writer setup configuration

--- setup.py
+++ setup.py
@@ -2,12 +2,23 @@ import setuptools
 
 
 setuptools.setup(
-    name="detection-to-json",
+    name="mqtt-writer",
     version="0.0.1",
     author="NovaVision AI",
     author_email="info@novavision.ai",
-    description="Convert NovaVision detections to JSON string",
+    description="MQTT Writer component for NovaVision",
+    url="https://github.com/Duyguersoy/cap-mqtt-writer",
     license="MIT",
+
+    install_requires=[
+        "paho-mqtt==2.1.0",
+    ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 
     packages=[
         "novavision.package",
